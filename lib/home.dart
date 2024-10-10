c38aec7714adfc41a7c1d3fb3f8f7b1654c73887
unknown <you@example.com>
2024-10-10 17:03:53 +0300

    first commit

---
+++ lib/home.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF0303030),
+      appBar: AppBar(
+        backgroundColor: Color(0xFF0303030),
+        title: const Text(
+          'Notes',
+          style: TextStyle(color: Colors.white),
+        ),
+        actions: [
+          Container(
+              margin: EdgeInsets.only(right: 10),
+              height: 40,
+              width: 40,
+              decoration: BoxDecoration(
+                  color: Color(0xFF3B3B3),
+                  borderRadius: BorderRadius.circular(10)),
+              child: IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.search,
+                    color: Colors.white,
+                  )))
+        ],
+      ),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Color(0xFFFFCD7A),
+                  borderRadius: BorderRadius.circular(30)),
+              width: 100,
+              height: 200,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
