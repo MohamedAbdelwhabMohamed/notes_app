c8844306563abf94c0d5347c6a760e38134c7a83
mohamed abdelwahb <mohamedelwhab111gmail.com>
2024-10-20 08:07:34 +0300

    create note model

---
+++ lib/model/note_model.dart
@@ -0,0 +1,11 @@
+class NoteModel {
+  String title;
+  String sebtTitle;
+  String date;
+  int color;
+  NoteModel(
+      {required this.title,
+      required this.sebtTitle,
+      required this.date,
+      required this.color});
+}
