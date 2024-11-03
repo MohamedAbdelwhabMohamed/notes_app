84d2dd3822eb989aa4cbaa0645f0004aa7b4c5bb
mohamed abdelwahb <mohamedelwhab111gmail.com>
2024-11-03 23:07:59 +0200

    display notes purt one

--- lib/widget/notes_view_bady.dart
+++ lib/widget/notes_view_bady.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:opp/cubits/netes_cubit/notes_cubit.dart';
 import 'package:opp/widget/custom_appBar.dart';
 import 'package:opp/widget/notes_list_view.dart';
 
-class NotesViewBady extends StatelessWidget {
+class NotesViewBady extends StatefulWidget {
   const NotesViewBady({super.key});
+
+  @override
+  State<NotesViewBady> createState() => _NotesViewBadyState();
+}
+
+class _NotesViewBadyState extends State<NotesViewBady> {
+  @override
+  void initState() {
+    BlocProvider.of<NotesCubit>(context).fetchNotes();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
